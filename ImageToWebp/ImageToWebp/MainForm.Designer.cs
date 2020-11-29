5656255efa6f88835ab35f42c31778df6a0f48b4
mryp <picobox.org@gmail.com>
2020-11-29 15:43:23 +0900

    ファイル選択作成

---
+++ ImageToWebp/ImageToWebp/MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace ImageToWebp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.filePathRefButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.imageOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(114, 12);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(592, 27);
+            this.filePathTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "画像ファイル：";
+            // 
+            // filePathRefButton
+            // 
+            this.filePathRefButton.Location = new System.Drawing.Point(712, 11);
+            this.filePathRefButton.Name = "filePathRefButton";
+            this.filePathRefButton.Size = new System.Drawing.Size(88, 29);
+            this.filePathRefButton.TabIndex = 2;
+            this.filePathRefButton.Text = "参照";
+            this.filePathRefButton.UseVisualStyleBackColor = true;
+            this.filePathRefButton.Click += new System.EventHandler(this.filePathRefButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 56);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(788, 46);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "処理開始";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // imageOpenFileDialog
+            // 
+            this.imageOpenFileDialog.Filter = "画像ファイル|*.png;*.jpg;*.bmp|すべてのファイル|*.*";
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.WorkerSupportsCancellation = true;
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorker_DoWork);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorker_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorker_RunWorkerCompleted);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 119);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.filePathRefButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Image to Webp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainForm_FormClosing);
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button filePathRefButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.OpenFileDialog imageOpenFileDialog;
+        private System.ComponentModel.BackgroundWorker bgWorker;
+    }
+}
+
---
+++ ImageToWebp/ImageToWebp/MainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageToWebp
+{
+    public partial class MainForm : Form
+    {
+        protected class WorkerParam
+        {
+            public string FilePath { get; set; } = "";
+            public int Quality { get; set; } = 100;
+
+            public override string ToString()
+            {
+                return $"{FilePath},{Quality}";
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            Log.Info("アプリケーション起動");
+        }
+
+        private void mainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Log.Info("アプリケーション終了");
+        }
+
+        private void filePathRefButton_Click(object sender, EventArgs e)
+        {
+            if (imageOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePathTextBox.Text = imageOpenFileDialog.FileName;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (bgWorker.IsBusy)
+            {
+                return;
+            }
+
+            var filePath = filePathTextBox.Text;
+            if (string.IsNullOrEmpty(filePath))
+            {
+                MessageBox.Show("画像ファイルパスが指定されていません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("画像ファイルパスが存在しません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            startButton.Enabled = false;
+            bgWorker.RunWorkerAsync(new WorkerParam()
+            {
+                FilePath = filePath,
+                Quality = 90,
+            });
+        }
+
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (sender is not BackgroundWorker worker)
+            {
+                Log.Error("sender is not BackgroundWorker");
+                e.Result = "処理パラメーターエラー";
+                return;
+            }
+            if (e.Argument is not WorkerParam param)
+            {
+                Log.Error("e.Argument is not WorkerParam");
+                e.Result = "処理パラメーターエラー";
+                return;
+            }
+
+            worker.ReportProgress(1);
+            Log.Info($"処理開始 param={param}");
+
+            var isSuccess = imageToWebp(param.FilePath, param.Quality);
+
+            worker.ReportProgress(1, Path.GetFileName(param.FilePath));
+            Log.Info("処理完了");
+
+            var result = isSuccess ? "成功" : "失敗";
+            e.Result = $"処理結果 {result}";
+        }
+
+        private static bool imageToWebp(string filePath, int quality)
+        {
+
+
+            return true;
+        }
+
+        private void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+        }
+
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            startButton.Enabled = true;
+            if (e.Cancelled)
+            {
+                MessageBox.Show("キャンセルされました", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (e.Result == null)
+            {
+                MessageBox.Show("想定外のエラーが発生しました", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(e.Result.ToString(), "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
